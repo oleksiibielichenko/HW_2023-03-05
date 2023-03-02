44d27fe312e6af4262c7c986892789ea3df6eeb5
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-02 21:39:39 +0200

    start

---
+++ task1.py
@@ -0,0 +1,11 @@
+days_of_week = {
+    'Monday': 1,
+    'Tuesday': 2,
+    'Wednesday': 3,
+    'Thursday': 4,
+    'Friday': 5,
+    'Saturday': 6,
+    'Sunday': 7
+}
+number = input("Enter week day number: ")
+if number
