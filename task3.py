f0f829ea506487d4e0f5eaf39d47b0b7833e1c9d
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-02 22:34:34 +0200

    done 3

---
+++ task3.py
@@ -0,0 +1,10 @@
+
+number = int(input("Enter any number: "))
+
+if number > 0:
+    print("Number is positive"),
+else:
+    if number < 0:
+        print("Number is negative"),
+    else:
+        print("Number is equal to zero")
