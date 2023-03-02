6bf30318ff6ea4239eb22ad1759a10686496fea3
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-02 22:27:31 +0200

    done 2

--- task1.py
+++ task1.py
@@ -2,9 +2,9 @@
 days_of_week = ['Monday', 'Tuesday', 'Wednesday',
                 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-number = int(input("Enter week day number: "))
+number = int(input("Enter Day number: "))
 
 if number > 0 and number < 8:
     print(days_of_week[number - 1]),
 else:
-    print("Invalid data!")
+    print("Invalid Day number!")
--- task2.py
+++ task2.py
@@ -1,10 +1,10 @@
 
-month_of_year = ['', 'Tuesday', 'Wednesday',
-                 'Thursday', 'Friday', 'Saturday', 'Sunday']
+month_of_year = ['January', 'February', 'March', 'April', 'May', 'June',
+                 'July', 'August', 'September', 'Oktober', 'November', 'December']
 
-number = int(input("Enter year month number: "))
+number = int(input("Enter Month number: "))
 
-if number > 0 and number < 8:
+if number > 0 and number < 13:
     print(month_of_year[number - 1]),
 else:
-    print("Invalid data!")
+    print("Invalid Month number!")
